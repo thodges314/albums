b5063dc04b9acfc50604dce3837fba438e0cefa4
Thomas Hodges <thodges@javascriptonline.org>
2019-06-18 13:28:42 -0700

    ready to build album detail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,15 +3,23 @@ import {View, Text} from 'react-native'
 import axios from 'axios'
 
 class AlbumList extends Component {
-	constructor(props){
-		super(props)
+	state = {albums: []}
+
+	componentDidMount() {
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-			.then(response => console.log(response))
+			.then(response => this.setState({albums:response.data}))
 	}
+
+	renderAlbums =() => {
+		return this.state.albums.map(
+			album => <Text key={album.title}>{album.title}</Text>
+		)
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>Album list.</Text>
+				{this.renderAlbums()}
 			</View>
 		)
 	}
