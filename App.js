136e96b55f7ee2ada3c6d6864fab4f7dadb69fb4
Thomas Hodges <thodges@javascriptonline.org>
2019-06-18 09:45:06 -0700

    add albumlist

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {View} from 'react-native'
 import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
 
 export default function App() {
   return (
-      <Header headerText='albums'/>
-  );
+    <View>
+      <Header headerText={'albums'}/>
+      <AlbumList />
+    </View>
+  )
 }
